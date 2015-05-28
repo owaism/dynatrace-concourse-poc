fdd90b4dcc2cfa8e90776480ab6dbda219460221
Owais Mohamed <equa.monde@gmail.com>
2015-05-28 18:55:14 -0400

    added semvar

--- build-task.sh
+++ build-task.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 mvn install -B -fdynatrace-service-broker-source-code/pom.xml -Dmaven.test.skip=true
 rm -rf dynatrace-service-broker-source-code/target/dynatrace-service-broker.jar.*
+mv dynatrace-service-broker-source-code/target/dynatrace-service-broker.jar dynatrace-service-broker-source-code/target/dynatrace-service-broker-$1.jar
