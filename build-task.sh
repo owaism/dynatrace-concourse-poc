52af914fee0dd20af59fe9547ff0c5ca21dfb9db
Owais Mohamed <equa.monde@gmail.com>
2015-05-28 23:17:15 -0400

    correct the version number usage

--- build-task.sh
+++ build-task.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+version_num=`cat $1`
 mvn install -B -fdynatrace-service-broker-source-code/pom.xml -Dmaven.test.skip=true
 rm -rf dynatrace-service-broker-source-code/target/dynatrace-service-broker.jar.*
-mv dynatrace-service-broker-source-code/target/dynatrace-service-broker.jar dynatrace-service-broker-source-code/target/dynatrace-service-broker-$1.jar
+mv dynatrace-service-broker-source-code/target/dynatrace-service-broker.jar dynatrace-service-broker-source-code/target/dynatrace-service-broker-$version_num.jar
