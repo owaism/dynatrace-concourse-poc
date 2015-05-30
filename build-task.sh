98c88708b330fd82df95af988f74572b8fad0662
Owais Mohamed <equa.monde@gmail.com>
2015-05-30 11:09:52 -0400

    try outs

--- build-task.sh
+++ build-task.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 version_num=`cat $1`
-mvn install -B -fdynatrace-service-broker-source-code/pom.xml -Dmaven.test.skip=true
-rm -rf dynatrace-service-broker-source-code/target/dynatrace-service-broker.jar.*
-mv dynatrace-service-broker-source-code/target/dynatrace-service-broker.jar dynatrace-service-broker-source-code/target/dynatrace-service-broker-$version_num.jar
+mvn package -B -fdynatrace-service-broker-source-code/pom.xml -Dmaven.test.skip=true
+#rm -rf dynatrace-service-broker-source-code/target/dynatrace-service-broker.jar.*
+#mv dynatrace-service-broker-source-code/target/dynatrace-service-broker.jar dynatrace-service-broker-source-code/target/dynatrace-service-broker-$version_num.jar
+
+mv dynatrace-service-broker-source-code/target/dynatrace-service-broker.jar ./dynatrace-service-broker-version.jar
